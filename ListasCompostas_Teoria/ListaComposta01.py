8b35c4bc8c28defe78c4151a3907eddcab9b5e64
Akylles Barros <42787020+Akyllesbarros@users.noreply.github.com>
2021-06-19 18:14:14 -0300

    Update Lista Composta

--- ListasCompostas_Teoria/ListaComposta01.py
+++ ListasCompostas_Teoria/ListaComposta01.py
@@ -1,11 +1,22 @@
 # Crie uma lista composta que recebe 5 nomes e 5 idades de clientes, utilizando o for e o if, verifique quais clientes são maiores de idade e quais são menores de idade e mostre na tela a seguinte frase para cada um deles: 'Fulano é maior de idade' ou 'Fulana é menor de idade' e  também quantos são maiores e quantos são menores de idade.
 
 lista_nomes = []
-pessoas = []
-
+contIdadeMaior = 0
+contIdadeMenor = 0
 for i in range(4):
+    pessoas = []
     nome = input('Digite o nome: ')
-    idade = (input('Digite a idade: '))
+    idade = int(input('Digite a idade: '))
     pessoas.append(nome)
     pessoas.append(idade)
-    lista_clientes.append(pessoas[:])
+    lista_nomes.append(pessoas[:])
+    if idade >= 18 :
+        print(f'{nome} é maior de idade.')
+        contIdadeMaior += 1
+    elif idade < 18 : 
+        print(f'{nome} é menor de idade.')
+        contIdadeMenor += 1
+print(f' São {contIdadeMaior} maiores de idade e {contIdadeMenor} menores de idade! ')
+    
+    
+
